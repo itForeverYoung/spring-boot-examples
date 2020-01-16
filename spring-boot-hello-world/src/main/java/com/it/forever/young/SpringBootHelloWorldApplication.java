1f12c0db01bc3e699646cdf70a5e0e84e4d2447f
Zhang, Damon X. -ND <Damon.X.Zhang.-ND@disney.com>
2020-01-16 13:20:21 +0800

    init project

---
+++ spring-boot-hello-world/src/main/java/com/it/forever/young/SpringBootHelloWorldApplication.java
@@ -0,0 +1,16 @@
+package com.it.forever.young;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhanj566
+ */
+@SpringBootApplication
+public class SpringBootHelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+    }
+
+}
---
+++ spring-boot-mongo/src/main/java/com/it/forever/young/SpringBootMongoApplication.java
@@ -0,0 +1,13 @@
+package com.it.forever.young;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMongoApplication.class, args);
+    }
+
+}
