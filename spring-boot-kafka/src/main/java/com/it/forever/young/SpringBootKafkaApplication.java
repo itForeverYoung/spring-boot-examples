5c0b106480ab5e4ef3605c3b867542cee1a5f6a2
Zhang, Damon X. -ND <Damon.X.Zhang.-ND@disney.com>
2020-01-16 13:49:31 +0800

    init project update readme

---
+++ spring-boot-kafka/src/main/java/com/it/forever/young/SpringBootKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.it.forever.young;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKafkaApplication.class, args);
+    }
+
+}
